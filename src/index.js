7893854d648b8cfc391a2a8dfa31ea3f78672117
mintyPT <1147911+mintyPT@users.noreply.github.com>
2018-10-14 01:22:42 +0200

    q

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 import _ from "lodash";
 
+import Mover from "./containers/Mover";
+
 import Day from "./components/Day";
 import Days from "./components/Days";
 import Handle from "./components/Handle";
@@ -132,60 +134,6 @@ class EmployeeSegmentHandle extends React.Component {
       <Mover onMouseMove={onMouseMove}>
         <Handle {...props} ref={ref} />
       </Mover>
-    );
-  }
-}
-
-class Mover extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { active: false };
-  }
-
-  componentDidMount() {
-    // document.addEventListener("mousedown", this.mousedown);
-    document.addEventListener("mouseup", this.mouseup);
-    document.addEventListener("mousemove", this.mousemove);
-  }
-
-  componentWillUnmount() {
-    //document.removeEventListener("mousedown", this.mousedown);
-    document.removeEventListener("mouseup", this.mouseup);
-    document.removeEventListener("mousemove", this.mousemove);
-  }
-
-  onMouseDown = event => {
-    this.setState({ active: true });
-  };
-
-  mousedown = event => {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      this.setState({ active: true });
-    }
-  };
-
-  mouseup = event => {
-    this.setState({ active: false });
-  };
-  mousemove = event => {
-    if (this.state.active && this.props.onMouseMove) {
-      this.props.onMouseMove(event);
-    }
-  };
-
-  setWrapperRef = node => {
-    this.wrapperRef = node;
-    if (this.props.ref) {
-      this.props.ref(node);
-    }
-  };
-
-  render() {
-    const { props } = this;
-    return (
-      <div ref={this.setWrapperRef} onMouseDown={this.onMouseDown}>
-        {props.children}
-      </div>
     );
   }
 }
